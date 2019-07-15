590e3a7b995b231b296a5f20b4d6c39af1cdf56e
Nafis-Iqbal <nafisiqbal10053@gmail.com>
2019-07-15 17:58:20 +0600

    game done

---
+++ Masters Project2/Assets/Scripts/Game Controllers/GameplayController.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameplayController : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject pausePanel;
+
+	[SerializeField]
+	private Button restartGameButton;
+
+	[SerializeField]
+	private Text scoreText, pauseText;
+
+	public static int score;
+    public Text highscoreText;
+
+	void Start () {
+        if (!PlayerPrefs.HasKey("Score"))
+        {
+            PlayerPrefs.SetInt("Score", 0);
+        }
+        else
+        {
+            int Hscore = PlayerPrefs.GetInt("Score");
+            highscoreText.text = Hscore.ToString();
+        }
+		scoreText.text = score + "M";
+		StartCoroutine (CountScore());
+        score = 0;
+	}
+
+	IEnumerator CountScore() {
+		yield return new WaitForSeconds(0.6f);
+		score++;
+		scoreText.text = score + "M";
+		StartCoroutine (CountScore());
+	}
+
+	void OnEnable() {
+		PlayerDied.endGame += PlayerDiedEndTheGame;
+	}
+
+	void OnDisable() {
+		PlayerDied.endGame -= PlayerDiedEndTheGame;
+	}
+
+	void PlayerDiedEndTheGame() {
+        Debug.Log(score);
+
+
+        int highscore = PlayerPrefs.GetInt("Score");
+        
+        if (highscore < score)
+        {
+            Debug.Log("gg3");
+            highscoreText.text = score.ToString();
+            PlayerPrefs.SetInt("Score", score);
+        }
+
+
+
+        pauseText.text = "Game Over";
+		pausePanel.SetActive (true);
+		restartGameButton.onClick.RemoveAllListeners ();
+		restartGameButton.onClick.AddListener (() => RestartGame());
+		Time.timeScale = 0f;
+
+	}
+
+	public void PauseButton() {
+		Time.timeScale = 0f;
+		pausePanel.SetActive (true);
+		restartGameButton.onClick.RemoveAllListeners ();
+		restartGameButton.onClick.AddListener (() => ResumeGame());
+	}
+
+	public void GoToMenu() {
+		Time.timeScale = 1f;
+		Application.LoadLevel ("MainMenu");
+	}
+
+	public void ResumeGame() {
+		Time.timeScale = 1f;
+		pausePanel.SetActive (false);
+	}
+
+	public void RestartGame() {
+		Time.timeScale = 1f;
+		Application.LoadLevel ("Gameplay");
+	}
+
+} //GameplayController
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Masters Project2/Assets/Scripts/Player Scripts/PlayerJump.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerJump : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip jumpClip;
+
+    private float jumpForce = 12f, forwardForce = 0f;
+
+    private Rigidbody2D myBody;
+
+    private bool canJump, onAir;
+
+    private Button jumpBtn;
+
+    void Awake()
+    {
+        onAir = false;
+        myBody = GetComponent<Rigidbody2D>();
+
+        jumpBtn = GameObject.Find("Jump Button").GetComponent<Button>();
+
+        jumpBtn.onClick.AddListener(() => Jump());
+    }
+
+    void Update()
+    {
+        if (Mathf.Abs(myBody.velocity.y) == 0)
+        {
+            canJump = true;
+        }
+        if (onAir == true)
+        {
+            if (transform.localPosition.x < 0)
+            {
+                if (transform.localPosition.y > -1.0f)
+                {
+                    myBody.AddForce(new Vector2(125.0f, 0.0f));
+                    onAir = false;                  
+                }
+            }
+        }
+        if(transform.localPosition.x > 0)
+        {
+            float tempY = transform.localPosition.y;
+            transform.position = new Vector3(0.0f, tempY,-5.0f);
+        }
+    }
+
+    public void Jump()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            onAir = true;
+
+            AudioSource.PlayClipAtPoint(jumpClip, transform.position);
+
+            if (transform.position.x < 0)
+            {
+                forwardForce = 1f;
+            }
+            else
+            {
+                forwardForce = 0f;
+            }
+
+            //transform.Translate (Vector2.zero);
+            myBody.velocity = new Vector2(forwardForce, jumpForce);
+        }
+
+    }
+
+
+} //PlayerJump
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
